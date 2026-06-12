b5e9ec84e6db7f0fe8f26d77bd993ac0a0d388d7
Kevin <kevinmensah114@gmail.com>
2026-06-12 20:39:47 +0800

    Remove hero scroll arrow, add top padding for nav clearance

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -23,8 +23,5 @@
         </a>
       </div>
     </div>
-    <a class="hero__scroll reveal" href="#about" aria-label="Scroll to about" v-reveal="340">
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-    </a>
   </header>
 </template>
