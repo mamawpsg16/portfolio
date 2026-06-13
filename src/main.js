c3825324d89c26497fa26a7fa5fb47f1d4784d0d
Kevin <kevinmensah114@gmail.com>
2026-06-13 10:34:24 +0800

    fix: restore reveal.js import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { reveal } from './reveal-MISSING.js'
+import { reveal } from './reveal.js'
 
 createApp(App).directive('reveal', reveal).mount('#app')
