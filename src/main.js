a93d3e7822cc2c7904fee6ed0678df3145aa09aa
Kevin <kevinmensah114@gmail.com>
2026-06-13 10:31:55 +0800

    demo: break build to test CI failure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { reveal } from './reveal.js'
+import { reveal } from './reveal-MISSING.js'
 
 createApp(App).directive('reveal', reveal).mount('#app')
